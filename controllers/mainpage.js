d9023c31b6901a66b1ad9160bfaa7ab0572f32f8
saevans86 <sam_evans1986@hotmail.com>
2023-10-13 05:56:44 -0600

    blog info now pulling to handlebars, pending additional work on when user loads indivual blog for editing/dding new, as well as user comment system/models etc

--- controllers/mainpage.js
+++ controllers/mainpage.js
@@ -12,42 +12,40 @@ router.get('/', withAuth, async (req, res) => {
     });
 
 });
-
-router.get('/', async (req, res) => {
+router.get('/home', withAuth, async (req, res) => {
     try {
-        console.log('777777777777777777777777777')
-        const blogData = await Blog.findAll({
-            include: [
-                {
-                    model: User,
-                    attributes: ['name'],
-                },
-            ],
+        console.log('PULL ALL TEST');
+        const blogs = await Blog.findAll({
+            where: { user_id: req.session.user_id },
+            attributes: { exclude: ['password'] },
+            include: [{ model: User }]
         });
+        console.log(blogs);
 
-        const blogStuff = blogData.map((blog) => blog.get({ plain: true }));
-       
+        const blogData = blogs.map((blog) => blog.get({ plain: true }));
+
         res.render('home', {
-            blogStuff,
-            logged_in: req.session.logged_in
+            blogs: blogData, // Pass the data as an array of objects
+            logged_in: true,
         });
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
+
 router.get('/home', withAuth, async (req, res) => {
     try {
-        console.log(userDeets)
-        const userDeets = await User.findByPk(req.session.user_id, {
+   console.log('PULL 1 TEST        ')
+        const blogs = await Blog.findByPk(req.session.user_id, {
             attributes: { exclude: ['password'] },
-            include: [{ model: Blog }],
+            include: [{ model: User }],
         });
 
-        const user = userDeets.get({ plain: true });
-
+        const blog = blogs.get({ plain: true });
+console.log(blog)
         res.render('home', {
-            ...user,
+            ...blog,
             logged_in: true
         });
     } catch (err) {
@@ -62,7 +60,7 @@ router.get('/blog/:id', async (req, res) => {
             include: [{ model: User, attributes: ['name'] }],
         });
         const blogIdMap = blogDeetsById.get({ plain: true });
-
+        console.log(blogIdMap)
         res.render('blogpage', {  //used for rendering specific blogs on the blogpage
             ...blogIdMap,
             logged_in: req.session.logged_in
